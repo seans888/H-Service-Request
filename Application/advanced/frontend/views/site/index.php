3d7af446bef9f50bd9c6e30830605314f17ef5e7
John Rafael Furuya <jpfuruya@gmail.com>
2017-10-04 15:28:56 +0800

    Changed some text to fit what the app is all about.

--- Application/advanced/frontend/views/site/index.php
+++ Application/advanced/frontend/views/site/index.php
@@ -42,10 +42,7 @@ $this->title = 'Taal Vista Ticketing System';
             <div class="col-lg-4">
                 <h2>HK and Engineering</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+               <p>Operations such as House-keeping or maintainance are recorded in a databse and Reports can then be generate to see some statistics and data appropriate data.</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">See more about the deprtment</a></p>
             </div>
