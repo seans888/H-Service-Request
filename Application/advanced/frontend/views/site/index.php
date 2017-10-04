b2e72c137514bbe65fe68d208b78c3d3ad84e7b4
John Rafael Furuya <jpfuruya@gmail.com>
2017-10-04 15:13:05 +0800

    Changed the heading of the text in the homepage(index.php).

--- Application/advanced/frontend/views/site/index.php
+++ Application/advanced/frontend/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'Taal Vista Ticketing System';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Welcome!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
