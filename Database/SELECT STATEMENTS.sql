973ca61da56998dd280ee2d14a4262232dbbb496
chavezorly <chavez.orly23@gmail.com>
2017-10-08 18:17:43 +0800

    Trial for sql statement for (report)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -2,7 +2,7 @@
 /* For Engineering */
 Select type_name, count(ticket.ticket_type_id) as count
     from ticket_type
-           join ticket
+           left join ticket
             on (ticket_type.id = ticket_type.ticket_type_id)
                 where department_id = 1
                 group by ticket_type
