978bbf41d1ac7a4a3100cb1d3721af133478036f
chavezorly <chavez.orly23@gmail.com>
2017-10-08 17:54:22 +0800

    added values for department head position in engineering department

--- Database/hsrrs (with values).sql
+++ Database/hsrrs (with values).sql
@@ -67,6 +67,7 @@ INSERT INTO `employee` (`id`, `emp_lname`, `emp_fname`, `emp_mname`, `emp_contac
 (2015105, 'Peralta', 'Francis Dodi', 'Acosta', '096123789035', 5, 2),
 (2015106, 'Borlongan', 'Neil John', 'Ppppp', '099215814701', 1, 1),
 (2015107, 'Pedralvez', 'Benjamin', 'Telan', '09321654123', 2, 1),
+(2015108, 'Bregias', 'Kevin', 'Gggg', '09741852963', 3, 1),
 
 
 -- --------------------------------------------------------
