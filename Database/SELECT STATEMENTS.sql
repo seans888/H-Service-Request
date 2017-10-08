ec33e6e7f6cba235c2e69ab85afc591000e51c01
chavezorly <chavez.orly23@gmail.com>
2017-10-08 18:07:38 +0800

    added a .sql file that serves as a draft when finding for the proper select statement

---
+++ Database/SELECT STATEMENTS.sql
@@ -0,0 +1,16 @@
+/* Ticket Type Count */
+/* For Engineering */
+/* For Housekeeping */
+
+                
+/* Room Location Count */
+/* For Housekeeping */    
+/* For Engineering */
+
+/* Request Time Count */
+/* For Housekeeping */    
+/* For Engineering */
+
+/* Common Request Count */
+/* For Housekeeping */
+
