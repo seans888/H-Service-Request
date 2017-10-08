5ac7ed301180a9071c93a803b7b81112568f8b0e
chavezorly <chavez.orly23@gmail.com>
2017-10-08 18:12:39 +0800

    Trial for sql statement for (report)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -1,7 +1,8 @@
 /* Ticket Type Count */
 /* For Engineering */
-Select type_name, count(ticket.ticket_type_id)
-    from ticket
+Select type_name, count(ticket.ticket_type_id) as count
+    from ticket_type
+           join ticket            
                 where department_id = 1
 
 /* For Housekeeping */
