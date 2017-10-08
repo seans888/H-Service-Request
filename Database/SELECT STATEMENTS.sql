45de2826c7232d8d2431eeb200fc32d86a7a1823
chavezorly <chavez.orly23@gmail.com>
2017-10-08 18:15:34 +0800

    Trial for sql statement for (report)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -2,7 +2,7 @@
 /* For Engineering */
 Select type_name, count(ticket.ticket_type_id) as count
     from ticket_type
-          outer join ticket
+          right join ticket
             on (ticket_type.id = ticket_type.ticket_type_id)
                 where department_id = 1
    
