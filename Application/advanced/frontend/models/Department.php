3d82e082a59da104ea2133c66d29dfbdfa6ea283
John Rafael Furuya <jpfuruya@gmail.com>
2017-10-19 23:04:20 +0800

    Added a department model in frontend.

---
+++ Application/advanced/frontend/models/Department.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "department".
+ *
+ * @property integer $id
+ * @property string $dept_name
+ *
+ * @property Employee[] $employees
+ * @property HierarchyLevel[] $hierarchyLevels
+ * @property Ticket[] $tickets
+ */
+class Department extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'department';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['dept_name'], 'required'],
+            [['dept_name'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'dept_name' => 'Dept Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEmployees()
+    {
+        return $this->hasMany(Employee::className(), ['department_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getHierarchyLevels()
+    {
+        return $this->hasMany(HierarchyLevel::className(), ['department_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTickets()
+    {
+        return $this->hasMany(Ticket::className(), ['department_id' => 'id']);
+    }
+}
