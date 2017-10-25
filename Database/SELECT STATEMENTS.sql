e461dde5a131909497634a8505944cee7d56c404
chavezorly <chavez.orly23@gmail.com>
2017-10-25 18:26:42 +0800

    Preparing select statement

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -9,7 +9,11 @@
 
 
 /* Item Request Count */
-
+select tick_request as 'ITEM REQUEST', count(*) as COUNTS from ticket 
+WHERE ticket_type_ID = 3 
+group by tick_request 
+order by counts 
+desc limit 3
 
 
 
