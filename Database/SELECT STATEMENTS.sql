ee8ac5eca384662077bfb7d314915dffb9d3c598
chavezorly <chavez.orly23@gmail.com>
2017-10-25 18:29:18 +0800

    SELECT STATEMENT FOR ITEM REQUEST DONE

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -11,7 +11,7 @@
 /* Item Request Count */
 select tick_request as 'ITEM REQUEST', count(*) as COUNT from ticket 
 WHERE ticket_type_ID = 3 AND 
-tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -30 day) AND NOW() 
+tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 MONTH) AND NOW() 
 group by tick_request order by count desc limit 3
 
 
