e87aa8509b521a5d5a3ee53950e7e3c6c47f379e
chavezorly <chavez.orly23@gmail.com>
2017-10-25 18:23:19 +0800

    Preparing select statement

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -1,23 +1,3 @@
-/* Ticket Type Count */
-/* For Engineering */
-Select type_name, count(ticket.ticket_type_id) as count
-    from ticket_type
-          left join ticket
-            on (ticket_type.id = ticket.ticket_type_id)
-                where department_id = 1
-                group by ticket_type.id
-   
-/* For Housekeeping */
+/* MONTHLY REPORT FOR HOUSEKEEPING*/
 
-                
-/* Room Location Count */
-/* For Housekeeping */    
-/* For Engineering */
-
-/* Request Time Count */
-/* For Housekeeping */    
-/* For Engineering */
-
-/* Common Request Count */
-/* For Housekeeping */
 
