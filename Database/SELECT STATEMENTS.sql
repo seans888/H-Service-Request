3d586a1bcadb1438e5e78585f1d9e569dbf729f3
neilborlongan <johnneilborlongan@gmail.com>
2017-11-01 13:25:05 +0800

    Added type count for monthly report for housekeeping

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -1,17 +1,16 @@
 /* MONTHLY REPORT FOR HOUSEKEEPING*/
 
-
-
 /* Type count */
-
+Select type_name 'Type of Request', count(ticket.ticket_type_id)
+as count from ticket_type join ticket on(ticket_type.id=ticket.ticket_type_id) 
 
 
 
 
 /* Item Request Count */
-select tick_request as 'ITEM REQUEST', count(*) as COUNT from ticket 
-WHERE ticket_type_ID = 3 AND 
-tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 MONTH) AND NOW() 
+select tick_request as 'ITEM REQUEST', count(*) as COUNT from ticket
+WHERE ticket_type_ID = 3 AND
+tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 MONTH) AND NOW()
 group by tick_request order by count desc limit 3
 
 
