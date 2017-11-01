5bde392e9f710a48f6f131fe3aa35c9d6d159a36
rpmeer <reinanpmeer@gmail.com>
2017-11-01 22:10:45 +0800

    Added Housekeeping Weekly Report for SELECT Statements

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -103,3 +103,15 @@ left join ticket on (room.room_no = ticket.room_room_no)
 where ticket_type_id in ('1','2')
 AND tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 MONTH)
 AND NOW()  group by room.room_location order by count desc
+
+
+
+/* HOUSEKEEPING WEEKLY REPORT */
+
+/* Type count */
+
+
+/* Item Request */
+
+
+/* Assistance Request */
