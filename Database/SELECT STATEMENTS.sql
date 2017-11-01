e0cb891fd2e79124494741b7b4cd79143f52af43
robonifacio <pargfxstudio@gmail.com>
2017-11-01 22:55:06 +0800

    Added WHERE sql statement for non-electrical repair request (weekly report for engineering)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -152,4 +152,4 @@ AND NOW() group by tick_request order by count desc limit 3
 
 /* NON-Electrical Repair Request */
 select tick_request as 'NON-ELECTRICAL REPAIR REQUEST', count(*)
-as COUNT from ticket 
+as COUNT from ticket WHERE ticket_type_ID = 2
