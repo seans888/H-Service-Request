3d5da99d96c72dd1b0b17aa6d84305040d8cc12d
rpmeer <reinanpmeer@gmail.com>
2017-11-01 22:30:50 +0800

    Added sql statement BETWEEN in assistance request (weekly report for housekeeping)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -126,4 +126,5 @@ AND NOW() group by tick_request order by count desc limit 3
 
 /* Assistance Request */
 select tick_request as 'ITEM REQUEST', count(*)
-as COUNT from ticket WHERE ticket_type_ID = 4 
+as COUNT from ticket WHERE ticket_type_ID = 4
+AND tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 WEEK)
