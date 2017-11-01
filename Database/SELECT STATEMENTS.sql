74c04993356c225572ae396e174aaa6e8e85bb56
robonifacio <pargfxstudio@gmail.com>
2017-11-01 16:29:03 +0800

    Added WHERE sql statement for outstanding employee (monthly report for engineerning)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -91,6 +91,7 @@ AND NOW() group by ticket_type.id ORDER BY COUNT DESC)x
 /* Outstanding Employee */
 Select username, count(user.id)
 as count from user join ticket on (user.id = ticket.assigned_to)
+where ticket_type_id in ('1','2')
 
 
 /* Ticket count in location */
