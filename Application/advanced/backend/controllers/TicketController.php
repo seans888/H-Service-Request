cfec0447183cc7f277b1f8df6b6e9828fa9621df
faperalta <faperalta2@gmail.com>
2017-11-01 23:28:15 +0800

    added forbiddenexception rule in B.E

--- Application/advanced/backend/controllers/TicketController.php
+++ Application/advanced/backend/controllers/TicketController.php
@@ -9,6 +9,7 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
+use yii\web\ForbiddenHttpException;
 
 /**
  * TicketController implements the CRUD actions for Ticket model.
