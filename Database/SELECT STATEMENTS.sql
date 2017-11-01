0fbfa80fb0aea9d777f8ba8374efa5cd7fea838d
rpmeer <reinanpmeer@gmail.com>
2017-11-01 14:12:40 +0800

    Added BETWEEN to sql query of counts of all ticket (monthly report for housekeeping)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -22,6 +22,7 @@ Select 'Total', sum(count)
 from (Select type_name 'Type of Request Received', count(ticket.ticket_type_id)
 as count from ticket_type left join ticket on (ticket_type.id=ticket.ticket_type_id)
 where ticket_type_id IN(3, 4) AND tick_startDate
+BETWEEN DATE_ADD(NOW(), INTERVAL -1 MONTH)
 
 
 /* Outstanding Employee */
