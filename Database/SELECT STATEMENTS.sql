d627a35503e756154bd0414e3483365a2f02adc1
neilborlongan <johnneilborlongan@gmail.com>
2017-11-01 15:08:56 +0800

    complete sql query for electrical repair request (monthly report for engineering)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -69,6 +69,7 @@ AND NOW() group by ticket_type.id ORDER BY COUNT DESC
 select tick_request as 'ELECTRICAL REPAIR REQUEST',
 count(*) as COUNT from ticket WHERE ticket_type_ID = 1
 AND tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 MONTH)
+AND NOW() group by tick_request 
 
 
 /* NON-Electrical Repair Request */
