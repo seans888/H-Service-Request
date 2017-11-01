e55e803f863ea629859f6c10e0a726d7913cc176
neilborlongan <johnneilborlongan@gmail.com>
2017-11-01 13:33:44 +0800

    final item requehst count (monthly report for housekeeping)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -10,10 +10,10 @@ AND NOW() group by ticket_type.id ORDER BY COUNT DESC
 
 
 /* Item Request Count */
-select tick_request as 'ITEM REQUEST', count(*) as COUNT from ticket
-WHERE ticket_type_ID = 3 AND
-tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 MONTH) AND NOW()
-group by tick_request order by count desc limit 3
+select tick_request as 'ITEM REQUEST', count(*)
+as COUNT from ticket WHERE ticket_type_ID = 3
+AND tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 MONTH)
+AND NOW() group by tick_request order by count desc limit 3
 
 
 
