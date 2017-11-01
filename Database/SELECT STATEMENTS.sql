706e09a1d90feff1d149728ed820e40f130c4a85
rpmeer <reinanpmeer@gmail.com>
2017-11-01 22:21:41 +0800

    Added sql statement GROUP BY in item request (weekly report for housekeeping)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -121,7 +121,7 @@ AND NOW() group by ticket_type.id ORDER BY COUNT DESC
 select tick_request as 'ITEM REQUEST', count(*)
 as COUNT from ticket WHERE ticket_type_ID = 3
 AND tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 WEEK)
-
+AND NOW() group by tick_request 
 
 
 /* Assistance Request */
