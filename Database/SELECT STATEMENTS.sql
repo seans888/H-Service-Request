bee84c3e0f776efbee77fa4c6c342f50a52024b5
rpmeer <reinanpmeer@gmail.com>
2017-11-01 14:08:36 +0800

    Added sql query for counts of all ticket (monthly report for housekeeping)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -18,6 +18,9 @@ AND NOW() group by tick_request order by count desc limit 3
 
 
 /* Counts of all ticket */
+Select 'Total', sum(count)
+from (Select type_name 'Type of Request', count(ticket.ticket_type_id)
+as count from ticket_type join ticket on (ticket_type.id=ticket.ticket_type_id) 
 
 
 /* Outstanding Employee */
