16c3a8d1c05aa5438b66f8cffad0e6cb517aab66
neilborlongan <johnneilborlongan@gmail.com>
2017-11-01 22:41:56 +0800

    Added sql query for tyoe count (weekly report for engineering)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -135,6 +135,10 @@ AND NOW() group by tick_request order by count desc limit 3
 /* ENGINEERING WEEKLY REPORT */
 
 /* Type count */
+Select type_name 'Type of Request Received',
+count(ticket.ticket_type_id) as count from ticket_type
+join ticket on(ticket_type.id=ticket.ticket_type_id) 
+
 
 
 /* Electrical repair request */
