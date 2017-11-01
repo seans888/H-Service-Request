5a48225bbe8594b83be937f366aa0d7a38a67668
neilborlongan <johnneilborlongan@gmail.com>
2017-11-01 13:41:30 +0800

    complete sql query for ticket count in location (monthly report in housekeeping)

--- Database/SELECT STATEMENTS.sql
+++ Database/SELECT STATEMENTS.sql
@@ -25,9 +25,10 @@ AND NOW() group by tick_request order by count desc limit 3
 
 /* Ticket count in location */
 Select room_location as 'Location' , count(room.room_no)
-as count from room join ticket on (room.room_no = ticket.room_room_no)
+as count from room left join ticket on (room.room_no = ticket.room_room_no)
 where ticket_type_id in ('3','4')
 AND tick_startDate BETWEEN DATE_ADD(NOW(), INTERVAL -1 MONTH)
+AND NOW()  group by room.room_location 
 
 
 /* Rooms that requested more than one in housekeeping department */
