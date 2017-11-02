988d86347f9a65c4525639aa59e1ea06a9371c97
faperalta <faperalta2@gmail.com>
2017-11-02 21:22:59 +0800

    created _endform

---
+++ Application/advanced/backend/views/ticket/_endform.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Ticket */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ticket-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'room_room_no')->textInput(['readonly' => true]) ?>
+
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'End', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
