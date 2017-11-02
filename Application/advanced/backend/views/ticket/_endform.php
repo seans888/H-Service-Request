23ec65ab7ae04501aedb99a73f02f4b170bf9781
faperalta <faperalta2@gmail.com>
2017-11-02 21:23:45 +0800

    added ticket status field in _endform

--- Application/advanced/backend/views/ticket/_endform.php
+++ Application/advanced/backend/views/ticket/_endform.php
@@ -15,6 +15,8 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'room_room_no')->textInput(['readonly' => true]) ?>
 
     <?= $form->field($model, 'tick_request')->textInput(['readonly' => true]) ?>   
+    
+    <?= $form->field($model, 'tick_status')->textInput(['readonly' => true, 'value' => 'Closed']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'End', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
