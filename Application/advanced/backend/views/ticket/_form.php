1820605c0e9045bc74de5cebe692243df3e17621
John Rafael Furuya <jpfuruya@gmail.com>
2017-12-19 00:07:02 +0800

    Revised the form for dependent dropDownList.

--- Application/advanced/backend/views/ticket/_form.php
+++ Application/advanced/backend/views/ticket/_form.php
@@ -3,14 +3,16 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper; 
-use backend\models\Tickettype;
-use backend\models\Department;
-use backend\models\Room;
-use backend\models\User;
+use frontend\models\Tickettype;
+use frontend\models\Department;
+use frontend\models\Room;
+use frontend\models\User;
+use frontend\models\Request;
 use kartik\time\TimePicker;
 use kartik\datetime\DateTimePicker;
+
 /* @var $this yii\web\View */
-/* @var $model frontend\models\Ticket */
+/* @var $model backend\models\Ticket */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
@@ -18,64 +20,35 @@ use kartik\datetime\DateTimePicker;
 
     <?php $form = ActiveForm::begin(); ?>
 
-   
-
-
-    <?= $form->field($model, 'tick_status')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'tick_priority')->textInput(['maxlength' => true]) ?>
-
-
-    <?= $form->field($model, 'tick_description')->textArea(['maxlength' => 300, 'rows' => 6, 'cols' => 50])  ?>
-
-    <?= $form->field($model, 'tick_timelimit')->widget(TimePicker::classname(), [])?>
-
-
-
-
-
-     <?= $form->field($model, 'ticket_type_id')->dropDownList(
-        ArrayHelper::map(Tickettype::find()->all(),'id','type_name')    , 
-    ['prompt'=>'Select Information'] 
-    ) ?>
-
-
-
-         <?= $form->field($model, 'department_id')->dropDownList(
-        ArrayHelper::map(Department::find()->all(),'id','dept_name')    , 
-    ['prompt'=>'Select Information'] 
-    ) ?>
-
-
-
-
-         <?= $form->field($model, 'room_room_no')->dropDownList(
+    <?= $form->field($model, 'room_room_no')->dropDownList(
         ArrayHelper::map(Room::find()->all(),'room_no','room_no')    , 
     ['prompt'=>'Select Information'] 
     ) ?>
 
+   <?= $form->field($model, 'ticket_type_id')->dropDownList(
+        ArrayHelper::map(Tickettype::find()->all(),'id','type_name'), 
+    [
+    'prompt'=>'Select Tickettype',
+        'onchange'=>'$.post("index.php?r=request/lists&id='.'"+$(this).val(),function( data ){
+                     $( "select#ticket-request_req_id" ).html( data );
+                   }); '
+        ] ); ?>
+
+    <?= $form->field($model, 'request_req_id')->dropDownList(
+        ArrayHelper::map(Request::find()->all(),'req_id','req_name')    , 
+    [
+    'prompt'=>'Select Request',
+       
+    ] 
+    ); ?>
+
     
-         <?= $form->field($model, 'user_idCreated')->textInput(['readonly' => true, 'value' => Yii::$app->user->identity->id])
-     
-    ) ?>
-
-
-
-    <?= $form->field($model, 'user_id1Assigned')->dropDownList(
+    <?= $form->field($model, 'assigned_to')->dropDownList(
         ArrayHelper::map(User::find()->all(),'id','username')    , 
     ['prompt'=>'Select Information'] 
     ) ?>
 
-
-    <?= $form->field($model, 'user_id2closed')->dropDownList(
-        ArrayHelper::map(User::find()->all(),'id','username')    , 
-    ['prompt'=>'Select Information'] 
-    ) ?>
-
-
-      
-
-
+    <?= $form->field($model, 'tick_others')->textArea(['maxlength' => true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
