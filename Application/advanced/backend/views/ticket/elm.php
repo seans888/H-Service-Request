b73e46ad27074f403d2a79f20fc9ac4184f70bad
chavezorly <chavez.orly23@gmail.com>
2018-01-05 22:17:37 +0800

    code for the connection to database

--- Application/advanced/backend/views/ticket/elm.php
+++ Application/advanced/backend/views/ticket/elm.php
@@ -208,8 +208,37 @@ class TableRows extends RecursiveIteratorIterator {
         echo "</tr>" . "\n";
     } 
 } 
+$servername = "localhost";
+$username = "root";
+$password = "qwerty";
+$dbname = "ems3";
 
-?>
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare(""); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>     
+</div>
+</div>
+
+
+
+
+            </div>
 
       </body>
 
