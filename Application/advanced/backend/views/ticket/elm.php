e2a36d2ec6c30f88207211fed8e22cb43e3f9526
chavezorly <chavez.orly23@gmail.com>
2018-01-05 22:15:09 +0800

    and now, code for the third chart

--- Application/advanced/backend/views/ticket/elm.php
+++ Application/advanced/backend/views/ticket/elm.php
@@ -158,6 +158,34 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 </div>
 
+<div class="col-md-4 col-sm-4 col-xs-12" style="margin-left: 180px;">
+              <div class="x_panel tile fixed_height_320 overflow_hidden" style="height: 400px; width: 450px;">
+                <div class="x_title">
+                  <h2></h2>
+                  <div class="clearfix"></div>
+                </div>
+          <table width="100%">
+                <tr>
+                    <center><p style ="color:black"><p></p></center>
+                      <th style="width:70%;">
+                        <p>Range</p>
+                      </th>
+                      <th>
+                        <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
+                          <p class=""></p>
+                        </div>
+                      </th>
+                    </tr>
+                 </table>
+                <div class="x_content">
+              <div id="piechart3" style="width: 500px; height: 100%;margin-left: -40px;margin-top: -5px;"></div>
+                </div>
+              </div>
+<br>
+<br>
+<br>
+<br>
+<br>
 
       </body>
 
