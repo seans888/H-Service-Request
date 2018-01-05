e60f5d0b8a563bcb50dac85a3ecd729369629f1b
chavezorly <chavez.orly23@gmail.com>
2018-01-05 22:00:45 +0800

    modifying codes

--- Application/advanced/backend/views/ticket/elm.php
+++ Application/advanced/backend/views/ticket/elm.php
@@ -23,9 +23,14 @@ $this->params['breadcrumbs'][] = $this->title;
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
+  
 
 
   </head>  
       <body>  
+      	<button class="btn btn-lg btn-success" onClick="toggleVisibility('LM');return false;">Last Month</button>
+       <a href="javascript:forprint()">Click here to Print the Page</a>
+        </div>
 
       </body>
