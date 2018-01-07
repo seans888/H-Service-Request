ec1067e721941721d1688151167c91cbca7aaf94
John Rafael Furuya <jpfuruya@gmail.com>
2018-01-07 19:03:15 +0800

    Added some instructions in the ticket RESTFULAPI controller.

--- Application/advanced/backend/controllers/TicketrController.php
+++ Application/advanced/backend/controllers/TicketrController.php
@@ -3,6 +3,7 @@ namespace backend\controllers;
 
 use yii\rest\ActiveController;
 
+// This controller is for converting ticket data to JSON script to be used by the Escalation Managament System 
 class TicketrController extends ActiveController{
 	
     public $modelClass = 'backend\models\Ticket';
