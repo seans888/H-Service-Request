5c3b6e8db08efcdbb9d20798ab33cbd89c9f16a5
faperalta <faperalta2@gmail.com>
2018-01-08 19:35:07 +0800

    added data provider in backend

--- Application/advanced/backend/controllers/TicketController.php
+++ Application/advanced/backend/controllers/TicketController.php
@@ -5,6 +5,7 @@ namespace backend\controllers;
 use Yii;
 use backend\models\Ticket;
 use backend\models\TicketSearch;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
